055f3fa9c4e3f72ad43547a4b76b5d173fb14044
Brent <brmundt10@gmal.com>
2015-04-12 03:26:01 -0400

    json representation of .txt

---
+++ node/merge.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Brent on 4/12/2015.
+ */
+
+var freeTimeList = [];
+
+for (calendar in inputList) {
+    freeTimeList.push(JSON.parse("parse.js of each value"));
+}
+
+while (freeTimeList.length > 1) {
+    var freeTime1 = freeTimeList[0];
+    var freeTime2 = freeTimeList[1];
+    freeTimeList.remove(1); //make sure that is the index
+
+    var new_freetime = merege(freeTime1, freeTime2);
+
+    freeTimeList[0] = newFreeTime;
+}
+
+
+
+merge(a,b) {
+
+}
